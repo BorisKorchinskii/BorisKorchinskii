372073f15e0b1467c420856340ebf41c9ca67834
BorisKorchinskii <boris.korchinskiy@gmail.com>
2018-11-19 15:22:01 +0300

    JDI hw remarks corrected

---
+++ src/main/java/JDI/entities/MetalsAndColorsFormData.java
@@ -0,0 +1,13 @@
+package JDI.entities;
+
+
+public class MetalsAndColorsFormData {
+
+    public int[] summary;
+    public String[] elements;
+    public String color;
+    public String metals;
+    public String[] vegetables;
+}
+
+
