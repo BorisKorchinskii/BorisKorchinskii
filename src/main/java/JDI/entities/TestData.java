ce0549aa4183225fb72e4440352883a1eda18af3
Dmitry_Lebedev <Dmitry_Lebedev1@epam.com>
2018-11-19 13:21:43 +0300

    comments

--- src/main/java/JDI/entities/TestData.java
+++ src/main/java/JDI/entities/TestData.java
@@ -1,8 +1,8 @@
 package JDI.entities;
 
 
+// TODO Class name should be written with relation to it purpose.
 public class TestData {
-
     public int[] summary;
     public String[] elements;
     public String color;
